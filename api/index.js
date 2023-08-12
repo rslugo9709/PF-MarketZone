e587d4005ab9ee3925ad5e656477d59d1200c213
CarlaMelina <cmps03.5@gmail.com>
2023-08-12 15:34:22 -0300

    add mongo models and db

--- api/index.js
+++ api/index.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const { PORT_SERVER } = process.env || 3004;
+const { connectToDatabase} = require ("./src/dbMongodb.js")
 
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
   server.listen(PORT_SERVER, () => {
     console.log(`Sever raised on port: ${PORT_SERVER}. Enjoy It!`); // eslint-disable-line no-console
   });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
---
+++ api/src/dbMongodb.js
@@ -0,0 +1,19 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017'; 
+const dbName = 'mz_store'; 
+
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+    return client.db(dbName);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+    throw error;
+  }
+}
+
+module.exports = { connectToDatabase };
