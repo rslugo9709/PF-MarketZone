92a3f8a71dd9126b125d774afcd061a38e81ff47
Carla Parise <92234636+CarlaMelina@users.noreply.github.com>
2023-08-13 10:05:16 -0300

    Actualizar index.js

--- api/index.js
+++ api/index.js
@@ -1,17 +1,20 @@
 require('dotenv').config();
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-const { PORT_SERVER } = process.env || 3004;
+//const { conn } = require('./src/db.js');
+//const { PORT_SERVER } = process.env || 3004;
 const { connectToDatabase} = require ("./src/dbMongodb.js")
+const port = process.env.PORT_SERVER || 3004;
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
-  server.listen(PORT_SERVER, () => {
-    console.log(`Sever raised on port: ${PORT_SERVER}. Enjoy It!`); // eslint-disable-line no-console
-  });
-});
-
-app.listen(port, () => {
+//conn.sync({ alter: true }).then(() => {
+  //server.listen(PORT_SERVER, () => {
+   // console.log(`Sever raised on port: $/. 
+//{PORT_SERVER}. Enjoy It!`); 
+// eslint-disable-line //no-console
+//  });
+//});
+connectToDatabase.then(()=>{
+server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
-});
+});})
 
